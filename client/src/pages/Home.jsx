84a2c53b4357868c5a654310289022a2b06e89e4
Vedesh <vedesh.a.chavan@gmail.com>
2023-12-11 21:13:18 +0530

    11/12/2023

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,42 @@
-// import { Navbar } from "../components/Navbar";
+import "../index.css";
 
 export const Home = () => {
+
+
+
+
   return (
     <>
-      <h1>Hello Home page</h1>
+      <main>
+        <section className="selection-hero">
+          <div className="container grid grid-two-cols">
+            <div className="hero-content">
+              <p>bgryt</p>
+              <h1>Welcome</h1>
+              <p>bgryt</p>
+              <div className="btn btn-group">
+                <a href="/contact">
+                  <button className="btn">connect now</button>
+                </a>
+                <a href="/service">
+                  <button className="btn">learn more now</button>
+                </a>
+              </div>
+              {/* hero images */}
+              <div className="hero-image">
+                <img
+                  src="/images/channels4_profile.jpg"
+                  alt="coding together"
+                  width="400"
+                  height="500"
+                />
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
     </>
   );
 };
+
 
