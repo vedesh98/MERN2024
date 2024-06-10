9903bff9bb70e7fa50c1e162532859fd0199a4f7
Vedesh <vedesh.a.chavan@gmail.com>
2024-06-10 21:25:43 +0530

    test01

--- server/utils/db.js
+++ server/utils/db.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 require("dotenv").config()
-// const url = process.env.MONGODB_URI;
+const url = process.env.MONGODB_URI;
 // const url = "mongodb://0.0.0.0:27017/rrrr";
-const url = "mongodb://27.0.0.1:27017/rrrr1";
+// const url = "mongodb://27.0.0.1:27017/rrrr1";
 
 
 
